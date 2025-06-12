63174ab9861bc12561fb593033cb5cf1d820678f
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-06-12 21:50:48 +0500

    reload error resolved

--- socialdots/urls.py
+++ socialdots/urls.py
@@ -22,7 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
-    # path("__reload__/", include("django_browser_reload.urls")),
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 # Replace the conditional static serving with this
