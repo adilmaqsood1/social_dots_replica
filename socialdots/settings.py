57c8f92d6ad63daedcab0be1a1cbc0d9d44e0d4a
adilmaqsood1 <126709358+adilmaqsood1@users.noreply.github.com>
2025-06-12 21:59:49 +0500

    reload error resolved

--- socialdots/settings.py
+++ socialdots/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-2wx$m(yy7hf72+zq7&seb5)@@ri9l^e_59v#hbig7_c#6r4lc+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 
 ALLOWED_HOSTS = ['*']
